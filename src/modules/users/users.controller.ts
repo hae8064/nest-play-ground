6b6fb64f3a43e6f47e5a1a64b3928a3acbb9e203
Bonghee <bhlee@aivis.kr>
2025-03-20 23:22:31 +0900

    feat: 회원가입 및 전체 유저 리스트 함수 추가가

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/createUser.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  // 회원가입
+  @Post('signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.createUser(body);
+  }
 
   @Get()
   getUsers() {
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/createUser.dto';
 
 @Injectable()
 export class UsersService {
-  constructor() {}
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-  getUsers() {
-    return 'Users';
+  async getUsers() {
+    const users = await this.userRepository.find();
+    return users;
+  }
+
+  async createUser(body: CreateUserDto) {
+    const user = this.userRepository.create(body);
+    return this.userRepository.save(user);
   }
 }
